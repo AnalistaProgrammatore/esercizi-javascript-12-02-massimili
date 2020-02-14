e699ec683e97e68afc8efe36013b300901c2f6f8
Massimiliano Puca <max.puca@gmail.com>
2020-02-14 10:34:25 +0100

    secondo caricamento

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,15 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
+let n = 10
+let prodotto = 1
+function fattoriale(x) {
+    if (x === 1) {
+        return prodotto
+    }
+    else {
+        prodotto = prodotto * x
+        return fattoriale(x - 1)
+    }
+}
+console.log(fattoriale(n))
